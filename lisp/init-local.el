7ff5c01509afd6dee8c041be53bcb5d64ba1557f
Wenxi Jin <jwenxi@gmail.com>
2020-07-31 08:22:11 +0200

    Add counsel-gtags-mode to c/c++, java mode hook

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,15 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'counsel-gtags)
-  (with-eval-after-load 'counsel-gtags
-    (define-key counsel-mode-map (kbd "<f12>") 'counsel-gtags-dwim)))
+(add-hook 'c-mode-hook 'counsel-gtags-mode)
+(add-hook 'c++-mode-hook 'counsel-gtags-mode)
 
 (add-hook 'java-mode-hook
           (function
            (lambda ()
              (setq tab-width 4)
              )))
+(add-hook 'java-mode-hook 'counsel-gtags-mode)
+
+(when (maybe-require-package 'counsel-gtags)
+  (with-eval-after-load 'counsel-gtags
+    (define-key counsel-gtags-mode-map (kbd "<f12>") 'counsel-gtags-dwim)))
 
 (provide 'init-local)
 ;;; init-local.el ends here
